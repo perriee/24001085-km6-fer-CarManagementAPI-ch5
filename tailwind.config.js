1c5c36e1d128f331d20de78f0845a69c93dbb0aa
Ferry Febriansyah <perriebenhard@gmail.com>
2024-03-29 15:46:31 +0700

    Finishing UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
     content: ["./public/**/*.html", "./node_modules/flowbite/**/*.js"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: "#922F93",
+                    hover: "#79197D",
+                },
+            },
+        },
     },
     plugins: [require("flowbite/plugin")],
 };
