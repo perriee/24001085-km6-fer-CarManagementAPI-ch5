141f03890a068927e5262cd6fc30f70fad727d3c
Ferry Febriansyah <perriebenhard@gmail.com>
2024-04-19 13:43:49 +0700

    feat: make user table and seeder

---
+++ migrations/20240419063641-create-user.js
@@ -0,0 +1,43 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("Users", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            email: {
+                type: Sequelize.STRING,
+            },
+            password: {
+                type: Sequelize.STRING,
+            },
+            name: {
+                type: Sequelize.STRING,
+            },
+            role: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                defaultValue: "user",
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            deletedAt: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("Users");
+    },
+};
